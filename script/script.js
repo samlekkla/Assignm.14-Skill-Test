09c7e7fc1b84c5e4778dea311b8c76e3f0b533d4
samlekkla <sam.lekkla@gmail.com>
2024-05-30 23:40:37 +0200

    Code structure update
    The project structure was improved by moving script.js into a new script folder, and clear, specific commit messages were used. The HTML structure was adjusted by placing the header and footer outside the main tag and the search bar inside a form tag. Duplicate href attributes were removed to fix warnings. The scroll down text and arrow were combined into one link. Syntax errors like incorrect quotation marks and an extra closing tag were corrected. Image icons for social links were replaced with Font Awesome icons, and hover effects were added. Hover effects were also added to the ‘book now’ buttons. To prevent the body from stretching, a maximum width was set. A layout issue with the footbar on page 2 for certain screen widths was fixed to improve responsiveness.

--- script/script.js
+++ script/script.js
@@ -1,13 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const menuToggle = document.querySelector('.menu-toggle');
+    const menuToggle = document.getElementById('menuToggle');
     const navList = document.querySelector('.nav__list');
-
-    menuToggle.addEventListener('click', function() {
-        navList.classList.toggle('active');
-    });
-
     const scrollDownIcon = document.querySelector('.fa-angle-double-down');
 
+    menuToggle.addEventListener('click', () => {
+        navList.classList.toggle('active');
+    });
+    
     scrollDownIcon.addEventListener('click', function() {
         const page2Container = document.querySelector('.page2__container');
         if (page2Container) {
